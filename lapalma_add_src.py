775c175f22c34184582263273be0a28bf997ec0d
hestetun <91600842+hestetun@users.noreply.github.com>
2023-12-13 16:40:34 +0100

    Update lapalma_add_src.py

--- lapalma_add_src.py
+++ lapalma_add_src.py
@@ -4,6 +4,8 @@ import DaVinciResolveScript as dvr_script
 
 source_folder = '/Volumes/lapalma_src/_src/'
 resolve = dvr_script.scriptapp('Resolve')
+print("Successfully accessed Resolve")  # <-- New line here
+
 project_manager = resolve.GetProjectManager()
 current_project = project_manager.GetCurrentProject()
 
@@ -13,10 +15,20 @@ media_pool = current_project.GetMediaPool()
 # Get MediaStorage API object
 media_storage = resolve.GetMediaStorage()
 
+file_list=[] 
+
 for day_dir in glob.glob(os.path.join(source_folder, 'DAY_*')):
     ocn_dir_path = os.path.join(day_dir, '01_OCN')
     if os.path.exists(ocn_dir_path):
-        # Add items in the 01_OCN directory to the media pool.
+        # Add items in the 01_OCN directory to a list.
         for file_name in os.listdir(ocn_dir_path):
             full_file_name=os.path.join(ocn_dir_path,file_name)
-            media_storage.AddItemsToMediaPool(full_file_name)
+            file_list.append(full_file_name)
+            print(f"File added: {full_file_name}")  # <-- New line here
+
+print("All files added. Importing into Media Pool...")  # <-- New line here
+
+# Add list of files to media pool all at once            
+media_storage.AddItemsToMediaPool(file_list)
+
+print("All files imported successfully!")   # <-- New line here
