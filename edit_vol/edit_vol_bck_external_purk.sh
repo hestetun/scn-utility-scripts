2fa973a8e4db5987a1a618794c3230e5f453dfa5
Ole-Andrè Hestetun <ole@shortcutoslo.no>
2024-01-02 11:37:22 +0100

    fixd log-name

--- edit_vol/edit_vol_bck_external_purk.sh
+++ edit_vol/edit_vol_bck_external_purk.sh
@@ -36,7 +36,7 @@ TODAY="$(date '+%y%m%d_%H%M')"
 STODAY="$(date '+%y%m%d')"
 LOGDIR=~/Library/Logs/
 $MKDIR -p $LOGDIR/editvol_bck # this line creates the directory if it does not exist
-LOGF=$LOGDIR/editvol_bck/edit_vol_bck_$TODAY.log
+LOGF=$LOGDIR/editvol_bck/edit_vol_bck_$PROJ_NAME"_"$TODAY.log
 EXCLUDE_LIST=~/git/editvol_bck/edit_exclude.txt
 EMAIL_ADRESS=scntech@shortcutoslo.no
 SINGLE_VOLS=`mount | $GREP "production" | $AWK '{print substr($3, 10)}'` # specific for purk
